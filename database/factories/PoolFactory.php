992b864cc37d2061352b08aa0a2ef56c6caf68f5
unknown <fajri.fadhila@gmail.com>
2026-03-04 01:08:36 +0700

    factory

---
+++ database/factories/PoolFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pool>
+ */
+class PoolFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'venue_id' => '',
+            'code' => '',
+            'name' => '',
+        ];
+    }
+}
